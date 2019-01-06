db1d3b5290d4c0ba76d8e119171e510c15cafeb9
ALBERTO SANCHEZ ORDONEZ <Alberto.Sanchez36@alu.uclm.es>
2019-01-06 13:33:38 +0100

    Hill Climbing finished

--- Practica 3/src/algorithms/HillClimbing.java
+++ Practica 3/src/algorithms/HillClimbing.java
@@ -5,27 +5,68 @@
  */
 package algorithms;
 
+import java.util.ArrayList;
+import optimization.Configuration;
 import optimization.OptimizationAlgorithm;
 
 /**
  *
  * @author Alberto
  */
-public class HillClimbing extends OptimizationAlgorithm  {
+public class HillClimbing extends OptimizationAlgorithm {
 
     @Override
     public void search() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        initSearch();
+
+        Configuration currentSolution = problem.genRandomConfiguration();
+        double currentScore = evaluate(currentSolution);
+        boolean improves = true;
+
+        while (improves) {
+            improves = false;
+            ArrayList<Configuration> neighbours = generateNeighbours(currentSolution);
+
+            for (Configuration neighbour : neighbours) {
+                double score = this.evaluate(neighbour);
+                if (score > currentScore) {
+                    currentSolution = neighbour;
+                    currentScore = score;
+                    improves = true;
+                }
+            }
+        }    
+        
+        stopSearch();
+
     }
 
     @Override
     public void showAlgorithmStats() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return;
     }
 
     @Override
     public void setParams(String[] args) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return;
     }
-    
+
+    private ArrayList<Configuration> generateNeighbours(Configuration currentSolution) {
+        ArrayList<Configuration> neighbours = new ArrayList<Configuration>();
+        
+        /* Swap the values of the configuration to get a new neighbour */
+        for(int i = 0; i < currentSolution.getValues().length; i++){
+            for(int j = i+1; j < currentSolution.getValues().length; j++){
+                int[] values = currentSolution.getValues().clone();
+                int temp = values[i];
+                values[i] = values[j];
+                values[j] = temp;
+                neighbours.add(new Configuration(values));
+            }
+        }
+        
+        
+        return neighbours;
+    }
+
 }
