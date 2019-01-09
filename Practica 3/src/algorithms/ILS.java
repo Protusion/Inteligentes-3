60df5d3581de53607e720729b7e9d5f7547bd396
ALBERTO SANCHEZ ORDONEZ <Alberto.Sanchez36@alu.uclm.es>
2019-01-09 23:54:59 +0100

    ILS works!!!

--- Practica 3/src/algorithms/ILS.java
+++ Practica 3/src/algorithms/ILS.java
@@ -6,7 +6,7 @@
 package algorithms;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Random;
 import optimization.Configuration;
 import optimization.OptimizationAlgorithm;
 
@@ -14,37 +14,32 @@ import optimization.OptimizationAlgorithm;
  *
  * @author Alberto
  */
-public class RLS extends OptimizationAlgorithm {
+public class ILS extends OptimizationAlgorithm {
 
     private int iterations = 20;
-    private ArrayList<Configuration> localOptimas = new ArrayList<Configuration>();
-    
+
+    private Random r = new Random();
+
     @Override
     public void search() {
-        
-        
+
         initSearch();
 
+        Configuration currentSolution = problem.genRandomConfiguration();
+
         int i = 0;
-        while(i < iterations){
-            Configuration randomStart = problem.genRandomConfiguration();
-            Configuration localOptima = hillClimbing(randomStart);
-            evaluate(localOptima);
-            localOptimas.add(localOptima);
+        while (i < iterations) {
+            currentSolution = perturbate(currentSolution);
+            currentSolution = hillClimbing(currentSolution);
             i++;
         }
-        
+
         stopSearch();
     }
 
     @Override
     public void showAlgorithmStats() {
-        System.out.print("Local optimas: ");
-        Collections.sort(localOptimas);
-        for(Configuration localOptima : localOptimas){
-            System.out.print(localOptima.score() + ", ");
-        }
-        System.out.println();
+        return;
     }
 
     @Override
@@ -57,8 +52,30 @@ public class RLS extends OptimizationAlgorithm {
             }
         }
     }
-    
-    private Configuration hillClimbing(Configuration configuration){
+
+    private Configuration perturbate(Configuration configuration) {
+        int[] pert = configuration.getValues().clone();
+        if (configuration.getValues().length < 3) { return configuration; }
+        int bound = configuration.getValues().length - 1;
+        
+        int r1 = r.nextInt(bound);
+        int r2 = r.nextInt(bound);
+        int r3 = r.nextInt(bound);
+        
+        while (r1 == r2 || r1 == r3 || r2 == r3){
+            r1 = r.nextInt(bound);
+            r2 = r.nextInt(bound);
+            r3 = r.nextInt(bound);
+        } 
+
+        pert[r1] = configuration.getValues()[r3];
+        pert[r2] = configuration.getValues()[r1];
+        pert[r3] = configuration.getValues()[r2];
+
+        return new Configuration(pert);
+    }
+
+    private Configuration hillClimbing(Configuration configuration) {
         Configuration currentSolution = configuration.clone();
         double currentScore = evaluate(currentSolution);
         boolean improves = true;
@@ -77,23 +94,21 @@ public class RLS extends OptimizationAlgorithm {
             }
         }
         return currentSolution;
-    }        
+    }
 
-    
     private ArrayList<Configuration> generateNeighbours(Configuration currentSolution) {
         ArrayList<Configuration> neighbours = new ArrayList<Configuration>();
         int[] values = currentSolution.getValues().clone();
         /* Swap the values of the configuration to get a new neighbour */
-        for(int i = 0; i < currentSolution.getValues().length; i++){
-            for(int j = i+1; j < currentSolution.getValues().length; j++){
+        for (int i = 0; i < currentSolution.getValues().length; i++) {
+            for (int j = i + 1; j < currentSolution.getValues().length; j++) {
                 values = currentSolution.getValues().clone();
                 values[i] = currentSolution.getValues()[j];
                 values[j] = currentSolution.getValues()[i];
                 neighbours.add(new Configuration(values));
             }
         }
-        
-        
+
         return neighbours;
     }
 
