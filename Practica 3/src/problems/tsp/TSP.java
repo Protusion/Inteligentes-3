78b66da2200c2a4036da61393815f91e9c4c5d5a
ALBERTO SANCHEZ ORDONEZ <Alberto.Sanchez36@alu.uclm.es>
2019-01-12 11:43:32 +0100

    Comments added to score function on TSP and MazeTSP

--- Practica 3/src/problems/tsp/TSP.java
+++ Practica 3/src/problems/tsp/TSP.java
@@ -106,16 +106,20 @@ public class TSP extends OptimizationProblem implements ProblemVisualizable {
     @Override
     public double score(Configuration configuration) {
 
+        /* Obtains the order of the cities */
         int[] values = configuration.getValues().clone();
 
         double score = 0;
 
+        /* Calculates the distance between the starting point and the first city */
         score += dist(posAgent, posCities.get(values[0]));
 
+        /* Calculates the addition of the distances between cities in order */
         for (int i = 0; i < values.length - 1; i++) {
             score += dist(posCities.get(values[i]), posCities.get(values[i + 1]));
         }
 
+        /* Calculates the distance between the last city and the final postion */
         score += dist(posCities.get(values[values.length - 1]), posExit);
 
         return score;
--- Practica 3/src/problems/tsp/maze/MazeTSP.java
+++ Practica 3/src/problems/tsp/maze/MazeTSP.java
@@ -40,23 +40,27 @@ public class MazeTSP extends TSP implements ProblemVisualizable {
     @Override
     public double score(Configuration configuration) {
 
+        /* Obtains the order of the cities */
         int[] values = configuration.getValues().clone();
 
         double score = 0;
 
+        /* Calculates the distance between the starting point and the first city */
         score += dist(posAgent, posCities.get(values[0]));
 
+        /* Calculates the addition of the distances between cities in order */
         for (int i = 0; i < values.length - 1; i++) {
             score += dist(posCities.get(values[i]), posCities.get(values[i + 1]));
         }
 
+        /* Calculates the distance between the last city and the final postion */
         score += dist(posCities.get(values[values.length - 1]), posExit);
 
         return score;
     }
 
     /**
-     * Calculates the distance between two points.
+     * Calculates the distance between two points (Manhattan)
      */
     private double dist(Position from, Position to) {
         return Math.abs(from.x - to.x) + Math.abs(from.y - to.y);
